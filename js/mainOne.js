58ef824af30e1ce31185e4ec3c66b6d82fee08f5
Ahmed Shawareb <ahmedshawareb567@gmail.com>
2020-02-19 06:44:01 -0800

    :)

--- js/mainOne.js
+++ js/mainOne.js
@@ -25,4 +25,37 @@ jQuery(function($) {
   $(window).resize(function() {
     ulDim = currentUl.offset();
   });
+  (function animateText() {
+    let textEl = [
+        "Animated Hover Menu",
+        "Hover Yes, Ok",
+        "Hover No, Yup",
+        "Ok Dont Mind"
+      ],
+      text = "",
+      newArr = [],
+      z = 0,
+      y = 0,
+      ind = 0;
+    for (let x = 0; x < textEl.length; x++) {
+      let splitElement = textEl[x].split("");
+      newArr.push(splitElement);
+    }
+    setInterval(function() {
+      if (z >= newArr.length) {
+        z = 0;
+        y = 0;
+        text = "";
+      } else {
+        if (y >= newArr[z].length) {
+          y = 0;
+          text = "";
+          z++;
+        }
+        text += newArr[z][y];
+        $(".printElement").text(text);
+        y++;
+      }
+    }, 400);
+  })();
 });
